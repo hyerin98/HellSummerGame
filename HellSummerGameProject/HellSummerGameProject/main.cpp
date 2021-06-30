ba55dd2e120f8a40aeaf166c93c361044fad034d
hyerin98 <cccccc0327@naver.com>
2021-06-30 23:23:26 +0900

    bubblebobble(left,right move)

--- HellSummerGameProject/HellSummerGameProject/main.cpp
+++ HellSummerGameProject/HellSummerGameProject/main.cpp
@@ -6,19 +6,18 @@ int main()
 {
     RenderWindow window(sf::VideoMode(1000, 1000), "Window");
 
-    Sprite sp;
+    Sprite Stage;
     Texture tx;
     tx.loadFromFile("Textures/pinkmap.png");
-    sp.setScale(3.0f, 3.0f);
-    sp.setOrigin(tx.getSize().x / 2.f, tx.getSize().y / 2.f);
-    sp.setTexture(tx);
-    sp.setPosition(500.f, 500.f);
-
+    Stage.setScale(3.0f, 3.0f);
+    Stage.setOrigin(tx.getSize().x / 2.f, tx.getSize().y / 2.f);
+    Stage.setTexture(tx);
+    Stage.setPosition(500.f, 500.f);
 
     Sprite bubble;
     Texture ob;
 
-    ob.loadFromFile("Textures/bubblebobble.png");
+    ob.loadFromFile("Textures/bubblebobbleright.png");
     bubble.setScale(0.4f, 0.4f);
     bubble.setOrigin(ob.getSize().x / 2.f, ob.getSize().y / 2.f);
     bubble.setTexture(ob);
@@ -29,7 +28,7 @@ int main()
     {
         window.clear();
 
-        window.draw(sp);
+        window.draw(Stage);
         window.draw(bubble);
 
         Event e;
@@ -44,10 +43,12 @@ int main()
             case Event::KeyPressed:
                 if (Keyboard::isKeyPressed(Keyboard::Left) == true)
                 {
+                    ob.loadFromFile("Textures/bubblebobbleleft.png");
                     bubble.move(-10.0f, 0.0f);
                 }
                 else if (Keyboard::isKeyPressed(Keyboard::Right) == true)
                 {
+                    ob.loadFromFile("Textures/bubblebobbleright.png");
                     bubble.move(10.0f, 0.0f);
                 }
                 break;
