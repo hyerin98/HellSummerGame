def78504361dfc3fb68abaf20ef96bdaaaf16ddd
hyerin98 <71120607+hyerin98@users.noreply.github.com>
2021-07-15 10:18:28 +0900

    바운딩박스 하는 법

--- HellSummerGameProject/HellSummerGameProject/main.cpp
+++ HellSummerGameProject/HellSummerGameProject/main.cpp
@@ -3,55 +3,63 @@
 
 int main()
 {
-	RenderWindow window(VideoMode(600, 600),"Window");
-	window.setFramerateLimit(60);
-	Event evt;
+	Engine engine;
 
-	Texture tx;
-	tx.loadFromFile("Textures/test.png");
+	engine.Render();
 
-	Sprite sp;
-	sp.setPosition(0, 300);
-	sp.setTexture(tx);
-	sp.setOrigin(sp.getGlobalBounds().width / 2.f, sp.getGlobalBounds().height / 2.f);
+	engine.Destroy();
 
-	cout << sp.getGlobalBounds().width << " / " << sp.getGlobalBounds().height << endl;
+	// 바운딩 박스
+	//RenderWindow window(VideoMode(600, 600), "Window");
+	//window.setFramerateLimit(60);
+	//Event evt;
+	//Engine engine;
 
-	Vertex vertice[5];
+	//Texture tx;
+	//tx.loadFromFile("Textures/test.png");
+	//engine.Render();
 
-	while (window.isOpen())
-	{
-		window.clear();
+	//Sprite sp;
+	//sp.setPosition(0, 300);
+	//sp.setTexture(tx);
+	//sp.setOrigin(sp.getGlobalBounds().width / 2.f, sp.getGlobalBounds().height / 2.f);
 
-		while (window.pollEvent(evt))
-		{
-			switch (evt.type)
-			{
-			case Event::Closed: 
-			{
-				window.close();
-				break;
-			}
-			default:
-				break;
-			}
-		}
+	//cout << sp.getGlobalBounds().width << " / " << sp.getGlobalBounds().height << endl;
 
-		/* Update */
+	//Vertex vertice[5];
 
-		sp.move(Vector2f(1.f, 0));
+	//while (window.isOpen())
+	//{
+	//	window.clear();
 
-		vertice[0] = Vertex(Vector2f(sp.getGlobalBounds().left,sp.getGlobalBounds().top) , Color::Red);
-		vertice[1] = Vertex(Vector2f(sp.getGlobalBounds().left + sp.getGlobalBounds().width , sp.getGlobalBounds().top), Color::Blue);
-		vertice[2] = Vertex(Vector2f(sp.getGlobalBounds().left + sp.getGlobalBounds().width, sp.getGlobalBounds().top + sp.getGlobalBounds().height), Color::Magenta);
-		vertice[3] = Vertex(Vector2f(sp.getGlobalBounds().left, sp.getGlobalBounds().top + sp.getGlobalBounds().height), Color::Green);
-		vertice[4] = vertice[0];
+	//	while (window.pollEvent(evt))
+	//	{
+	//		switch (evt.type)
+	//		{
+	//		case Event::Closed:
+	//		{
+	//			window.close();
+	//			break;
+	//		}
+	//		default:
+	//			break;
+	//		}
+	//	}
+
+	//	/* Update */
+
+	//	sp.move(Vector2f(1.f, 0));
+
+	//	vertice[0] = Vertex(Vector2f(sp.getGlobalBounds().left, sp.getGlobalBounds().top), Color::Red);
+	//	vertice[1] = Vertex(Vector2f(sp.getGlobalBounds().left + sp.getGlobalBounds().width, sp.getGlobalBounds().top), Color::Blue);
+	//	vertice[2] = Vertex(Vector2f(sp.getGlobalBounds().left + sp.getGlobalBounds().width, sp.getGlobalBounds().top + sp.getGlobalBounds().height), Color::Magenta);
+	//	vertice[3] = Vertex(Vector2f(sp.getGlobalBounds().left, sp.getGlobalBounds().top + sp.getGlobalBounds().height), Color::Green);
+	//	vertice[4] = vertice[0];
 
 
 
-		/* Render */
-		window.draw(sp);
-		window.draw(vertice, 5, LinesStrip);
-		window.display();
-	}
+	//	/* Render */
+	//	window.draw(sp);
+	//	window.draw(vertice, 5, LinesStrip);
+	//	window.display();
 }
